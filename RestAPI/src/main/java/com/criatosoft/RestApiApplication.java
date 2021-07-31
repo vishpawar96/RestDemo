4204685546625331578aff5deb8672d0d5e4451e
User <swapnilp.gceme@gmail.com>
2021-07-31 13:55:47 +0530

    Second commit

--- RestAPI/src/main/java/com/criatosoft/RestApiApplication.java
+++ RestAPI/src/main/java/com/criatosoft/RestApiApplication.java
@@ -9,6 +9,7 @@ public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 		System.out.println("this is main method");
+		System.out.println("This is sop statement");
 	}
 
 }
