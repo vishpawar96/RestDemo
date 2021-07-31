6993b9a53cb75ea9a3fb1694fbef3d475e5be874
Swapnil <swapnilp.gceme@gmail.com>
2021-07-31 14:04:05 +0530

    This is my commit

--- RestAPI/src/main/java/com/criatosoft/RestApiApplication.java
+++ RestAPI/src/main/java/com/criatosoft/RestApiApplication.java
@@ -8,8 +8,13 @@ public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+
 		System.out.println("this is main method");
 		System.out.println("This is sop statement");
+
+		System.out.println("this is main method------Swapnil");
+		System.out.println("this is main method--- Vitthal");
+
 	}
 
 }
